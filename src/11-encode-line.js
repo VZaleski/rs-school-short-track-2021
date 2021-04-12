97dd761b1443facf2b503adb01dc0ce7089f1f86
VZaleski <70795078+VZaleski@users.noreply.github.com>
2021-04-12 22:05:57 +0300

    feat: add task 11-encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let resultString = '';
+  let countSymbol = 1;
+  if (!str) {
+    return '';
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      countSymbol++;
+    } else if (countSymbol === 1) {
+      resultString += str[i];
+    } else {
+      resultString += `${countSymbol}${str[i]}`;
+      countSymbol = 1;
+    }
+  }
+  return resultString;
 }
 
 module.exports = encodeLine;
